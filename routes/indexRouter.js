b9865e5b7540dcbce900c049b864f64e2b07ef5e
junaid <jk7948677@gmail.com>
2026-07-22 22:38:55 +0530

    Added .get for every message

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,51 +1,45 @@
-// routes/indexRouter.js
+  // routes/indexRouter.js
 
-const { Router } = require('express')
-const indexRouter = Router()
+  const { Router } = require('express')
+  const indexRouter = Router()
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  },
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hi there!bhbeq3uhygcyu3gyucg2b4tcr782t3bc98t",
-    user: "Amando",
-    added: new Date()
-  }
-];
+  let msgCounter = 2
+
+  const messages = [
+    {
+      id: 2,
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      id: 1,
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
 
 
-indexRouter.get('/', (req, res) => {
-    res.render('index', { messages: messages })
-})
+  indexRouter.get('/', (req, res) => {
+      res.render('index', { messages: messages })
+  })
 
-indexRouter.get('/new', (req, res) => {
-  res.render('form')
-})
+  indexRouter.get('/new', (req, res) => {
+    res.render('form')
+  })
 
-indexRouter.post('/new', (req, res) => {
-  const message = req.body.msg
-  const user = req.body.user
+  indexRouter.get('/messages/:id', (req, res) => {
+    res.render('message', { msg: messages.find((m) => m.id === Number(req.params.id))  })
+  })
 
-  messages.push({ text: message, user: user, added: new Date() });
-  res.redirect('/')
-})
+  indexRouter.post('/new', (req, res) => {
+    const message = req.body.msg
+    const user = req.body.user
 
-module.exports = indexRouter
+    msgCounter++
+    messages.unshift({id: msgCounter, text: message, user: user, added: new Date() });
+    res.redirect('/')
+  })
+
+  module.exports = indexRouter
